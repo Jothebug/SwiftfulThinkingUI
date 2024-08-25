cbc6264f2a88ef84048c8b0e3882ebca8d9f136a
Jothebug <hayenbg2603@gmail.com>
2024-08-25 12:04:27 +0700

    Text

--- SwiftfulThinkingBootcamp/ContentView.swift
+++ SwiftfulThinkingBootcamp/ContentView.swift
@@ -10,10 +10,29 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, world!".capitalized)
+                .font(.title)
+                .fontWeight(.semibold)
+                .fontDesign(.monospaced)
+                .frame(width: 100, height: 100, alignment: .trailing)
+                .minimumScaleFactor(0.1)
+            
+            Text("Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it".lowercased())
+//                .font(.body)
+//                .fontWeight(.semibold)
+//                .bold()
+//                .underline()
+//                .underline(true, color: Color.red)
+//                .italic()
+//                .strikethrough(true, color: Color.green)
+            
+                .font(.system(size: 24, weight: .semibold, design: .serif))
+//                .baselineOffset(5)
+//                .kerning(10)
+                .multilineTextAlignment(.leading)
+                .foregroundColor(.red)
+                .frame(width: 100, height: 100, alignment: .center)
+                .minimumScaleFactor(0.1)
         }
         .padding()
     }
