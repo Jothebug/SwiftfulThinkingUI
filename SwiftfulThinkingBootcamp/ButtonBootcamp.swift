6f935f23da52ba55be70a8a5bb7363007c6cb8a6
Jothebug <hayenbg2603@gmail.com>
2024-08-27 22:10:40 +0700

    Button

---
+++ SwiftfulThinkingBootcamp/ButtonBootcamp.swift
@@ -0,0 +1,76 @@
+//
+//  ButtonBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by HaYen on 27/8/24.
+//
+
+import SwiftUI
+
+struct ButtonBootcamp: View {
+    @State var title: String = "This is my title"
+    
+    var body: some View {
+        VStack {
+            Text(title)
+            
+            // #1
+            Button("Press me!") {
+                self.title = "Button #1"
+            }
+            .accentColor(.red)
+            
+            // #2
+            Button(action: {
+                self.title = "Button #2"
+            }, label: {
+                Text("Save".uppercased())
+                    .font(.headline)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .padding()
+                    .padding(.horizontal, 20)
+                    .background(
+                        Color.blue
+                            .cornerRadius(10)
+                            .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                    )
+            })
+            // #3
+            Button(action: {
+                self.title = "Button #3"
+            }, label: {
+                Circle()
+                    .fill(.white)
+                    .frame(width: 100, height: 100, alignment: .center)
+                    .shadow(radius: 10)
+                    .overlay(
+                        Image(systemName: "heart.fill")
+                            .font(.largeTitle)
+                            .foregroundColor(.red)
+                    )
+            })
+            
+            // #4
+            Button(action: {
+                self.title = "#4"
+            }, label: {
+                Text("Finish".uppercased())
+                    .font(.caption)
+                    .bold()
+                    .foregroundColor(.gray)
+                    .padding()
+                    .padding(.horizontal, 20)
+                    .background(
+                        Capsule()
+                            .stroke(Color.gray, lineWidth: 2.0)
+                    )
+            })
+        }
+      
+    }
+}
+
+#Preview {
+    ButtonBootcamp()
+}
