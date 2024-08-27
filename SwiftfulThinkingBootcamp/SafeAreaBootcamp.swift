5b1965e1f8b8c246a20aaa59aa3b9699f5e6f342
Jothebug <hayenbg2603@gmail.com>
2024-08-27 21:30:43 +0700

    .

---
+++ SwiftfulThinkingBootcamp/SafeAreaBootcamp.swift
@@ -0,0 +1,35 @@
+//
+//  SafeAreaBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by HaYen on 27/8/24.
+//
+
+import SwiftUI
+
+struct SafeAreaBootcamp: View {
+    var body: some View {
+        ScrollView {
+            VStack {
+                Text("Title goes here")
+                    .font(.largeTitle)
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                
+                ForEach(0..<10) {index in
+                    RoundedRectangle(cornerRadius: 25.0)
+                        .fill(.white)
+                        .frame(height: 150)
+                        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                        .padding(20)
+                }
+            }
+        }
+        .background(Color.blue)
+//        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+//        .ignoresSafeArea(.container)
+    }
+}
+
+#Preview {
+    SafeAreaBootcamp()
+}
