bb2195ee9154bad09f8213b0e802cf7a32049016
Jothebug <hayenbg2603@gmail.com>
2024-09-02 22:09:50 +0700

    TextField Bootcamp

---
+++ SwiftfulThinkingBootcamp/TextFiledBootcamp.swift
@@ -0,0 +1,67 @@
+//
+//  TextFiledBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by HaYen on 2/9/24.
+//
+
+import SwiftUI
+
+struct TextFiledBootcamp: View {
+    
+    @State var textFieldText: String = ""
+    @State var dataArray: [String] = []
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                TextField("Type something here...", text: $textFieldText)
+        //            .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                    .background(Color.gray.opacity(0.3).cornerRadius(10))
+                    .foregroundColor(.red)
+                    .font(.headline)
+                
+                Button(action: {
+                    if textIsAppropriate() {
+                        saveText()
+                    }
+                }, label: {
+                    Text("Save".uppercased())
+                        .padding()
+                        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                        .background(textIsAppropriate() ?  Color.blue : Color.gray)
+                        .cornerRadius(10)
+                        .foregroundColor(.white)
+                        .font(.headline)
+                })
+                .disabled(!textIsAppropriate())
+                
+                ForEach(dataArray, id: \.self) { data in
+                    Text(data)
+                }
+                
+                Spacer()
+            }
+            .padding()
+            .navigationTitle("TextField Bootcamp")
+        }
+    }
+    
+    func textIsAppropriate() -> Bool {
+        if textFieldText.count >= 3 {
+            return true
+        }
+        
+        return false
+    }
+    
+    func saveText() {
+        dataArray.append(textFieldText)
+        textFieldText = ""
+    }
+}
+
+#Preview {
+    TextFiledBootcamp()
+}
