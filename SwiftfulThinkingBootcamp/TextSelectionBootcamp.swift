d72a4b554b4a05d996bb0467dafeffb2877a2f15
Jothebug <hayenbg2603@gmail.com>
2024-09-05 23:41:18 +0700

    TextSelection

---
+++ SwiftfulThinkingBootcamp/TextSelectionBootcamp.swift
@@ -0,0 +1,19 @@
+//
+//  TextSelectionBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by HaYen on 5/9/24.
+//
+
+import SwiftUI
+
+struct TextSelectionBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .textSelection(.enabled)
+    }
+}
+
+#Preview {
+    TextSelectionBootcamp()
+}
